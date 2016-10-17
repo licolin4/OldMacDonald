ea572a9464ab3233a52215c461cb8b368cb0fdc5
licolin4 <licolin4@gmail.com>
2016-10-17 11:37:41 -0700

    Going to The Farm

--- Cow.java
+++ Cow.java
@@ -1,7 +1,7 @@
 class Cow implements Animal 
 {     
-     public String myType;     
-     public String mySound;      
+     protected String myType;     
+     protected String mySound;      
      public Cow(String type, String sound)     {         
          myType = type;         
          mySound = sound;     
